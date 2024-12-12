6f220c553c0f9bb88bab3396a9c204c9917b8347
Sanjay Achar <94422583+M-Sanjay12o52o@users.noreply.github.com>
2024-12-12 07:42:17 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function CheckCoupons() {
 
       {/* // Displaying winnign coupon */}
       <div>
-        <h1 className="text-2xl font-bold text-green-600 mb-4 text-center">
+        <h1 className="text-2xl font-bold text-purple-600 mb-4 text-center">
           Winning Coupons:
         </h1>
         {winningCoupons && winningCoupons.length > 0 ? (
